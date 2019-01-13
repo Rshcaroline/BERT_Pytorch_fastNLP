340831deb0acf540b3a186ad17401a784605212d
aaronheee <aaronhe@Aaron-He.local>
2019-01-14 04:09:31 +0800

    fastNLP based BERT

--- README.md
+++
@@ -1,2 +0,0 @@
-# BERT_Pytorch_fastNLP
-A PyTorch &amp; fastNLP implementation of Google AI's BERT model.
--- reference/fastNLP-master/fastNLP/modules/decoder/__init__.py
+++
@@ -1,4 +0,0 @@
-from .CRF import ConditionalRandomField
-from .MLP import MLP
-
-__all__ = ["ConditionalRandomField", "MLP"]
